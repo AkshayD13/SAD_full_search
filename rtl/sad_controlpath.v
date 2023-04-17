336e0b4cdbe772c4f4ef46b4619857233330e64c
Akshay Dandekar <47881557+AkshayD13@users.noreply.github.com>
2023-04-17 22:28:29 +0530

    Updated SAD controlpath design using FSM
    Detailed discussion is required about the FSM transition from S4 to S0. Also, it is required to make the control path and data path logic separate in different Verilog files.
    Verilog code review as per Verilog coding guidelines is required.

--- rtl/sad_controlpath.v
+++ rtl/sad_controlpath.v
@@ -3,9 +3,9 @@ input rst,clk,go;
 output [31:0]sad_reg;
 output [8:0] i;
 
-wire[7:0] abs_difference,A,B;
+wire[7:0] diff,A,B;
 wire bout;
-wire [31:0]sum_output,sum_reg;
+wire [31:0]abs_diff;
 
 parameter [2:0] s0=3'b000;
 parameter [2:0] s1=3'b001;
@@ -14,58 +14,103 @@ parameter [2:0] s3=3'b011;
 parameter [2:0] s4=3'b100;
 reg[8:0] i;
 
-reg sum_clr,sum_load,i_clr,i_inc,i_lt256,sad_load;
-reg [2:0]p_state,n_state;
+reg sum_clr,sum_load,i_clr,i_inc,sad_load;
+reg [2:0]pst,nxt;
 reg [31:0]sum,sad_reg;
 
 //*********************************************************
-//# Controller #//
+//# CONTROLLER #//
 //*********************************************************
+always @ (posedge clk,posedge rst) //present state logic
+begin
+	if (rst) 
+		begin
+		pst<=s0;
+		end
+	else 
+		begin
+		pst<=nxt;
+		end
+end		
+
+always @ (*) //next state logic
+begin
+		case(pst)
+	
+		s0: nxt = (go==1)?s1:s0;
+	
+		s1: begin
+			sum_clr = 1'b1;
+			i_clr = 1'b1; 
+			nxt = s2;
+			end
+		
+		s2: begin
+			nxt = (i<256)?s3:s4;
+			end
+			
+		s3: begin
+			sum_load = 1'b1;
+			i_inc = 1'b1; 
+			nxt = s2;
+			end
+		
+		s4: begin
+			sad_load = 1'b1;
+			nxt = s0;
+			end
+	
+		endcase
+end
+//*********************************************************
+//# DATA PATH #//
+//*********************************************************
+         
+cla_8bit_subtractor x1 (A,B,diff,bout);
+
+always @ (posedge clk)
+begin
+	if(i_clr || (i==256)) 
+	begin
+		i<=1'b0;
+		i_clr<=1'b0;
+	end
+	else if(i_inc)
+	begin
+		i<=i+1;
+		i_inc<=1'b0;
+	end
+	else 
+		i<=i;
+end
 
 always @(posedge clk)
 begin
-	if(rst)
-	begin
-		p_state<=s0;
-	end
-	else
-	begin
-		p_state<=n_state;
-	end
+		if(sum_clr)
+		begin
+			sum<=32'b0;
+			sum_clr<=1'b0;
+		end
+		else if(sum_load) 
+		begin
+			sum<=sum+abs_diff;
+			sum_load<=1'b0;
+		end
+		else 
+			sum<=sum;	 
 end
 
-always @(*)
-begin	
-	case(p_state)
-	s0:
-	if(go==1)
-		n_state=s1;
-	else
-		n_state=s0;
-		
-	s1:
-	sum_clr=1;
-	i_clr=1;
-	n_state=s2;
-	
-	s2:
-	if(i<256)
-		n_state=s3;
-	else
-		n_state=s4;
-	
-	s3:
-	sum_load=1;
-	i_inc=1;
-	greater=(A>=B)?A:B;
-	smaller=(A>=B)?B:A;
-	abs_difference=greater-smaller;
-	sum_reg=sum_output+abs_difference;
-	n_state=s2;
+always @(posedge clk)
+begin
+	if(sad_load)
+	begin
+		sad_reg<=sum;
+		sad_load<=1'b0;
+	end
+	else 
+		sad_reg<=sad_reg;
 
-	s4:
-	sad_load=1;
-	n_state=s0;
-	
-	endcase
-end
+end                  
+assign abs_diff = {bout,diff};
+
+endmodule
