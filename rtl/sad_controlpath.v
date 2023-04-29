c60def5ad5307848c5279f1b91b922caa3f37e58
Akshay Dandekar <47881557+AkshayD13@users.noreply.github.com>
2023-04-29 21:37:33 +0530

    Updated SAD control path and data path logic
    data_load signal is added in order to read the data from the FIFO when the system is in the S2 state only and to prepare the data for SAD calculation in the S3 state

--- rtl/sad_controlpath.v
+++ rtl/sad_controlpath.v
@@ -1,9 +1,9 @@
-module sad_controlpath(A,B,clk,rst,go,sad_reg,i);
+module sad_controlpath(A,B,clk,rst,go,sad_reg,i,data_load);
 input [7:0] A,B;
 input rst,clk,go;
 output [31:0]sad_reg;
 output [8:0] i;
-
+output data_load;
 
 
 wire[7:0] diff;
@@ -17,7 +17,7 @@ parameter [2:0] s3=3'b011;
 parameter [2:0] s4=3'b100;
 reg[8:0] i;
 
-reg sum_clr,sum_load,i_clr,i_inc,sad_load;
+reg sum_clr,sum_load,i_clr,i_inc,sad_load,data_load;
 reg [2:0]pst,nxt;
 reg [31:0]sum,sad_reg;
 
@@ -38,6 +38,7 @@ end
 
 always @ (*) //next state logic
 begin
+		data_load = 1'b0;		//default value of data_load signal
 		case(pst)
 	
 		s0: nxt = (go==1)?s1:s0;
@@ -50,9 +51,11 @@ begin
 		
 		s2: begin
 			nxt = (i<256)?s3:s4;
+			data_load = 1'b1;	//data_load signal is used to load the data into reg A and B from FIFO data_out port to keep the data ready for the operations in state S3
 			end
 			
 		s3: begin
+			data_load = 1'b0;	//data_load is made 0 in order to stop the reading of the data from FIFO into reg A and B			
 			sum_load = 1'b1;
 			i_inc = 1'b1; 
 			nxt = s2;
@@ -73,7 +76,7 @@ cla_8bit_subtractor x1 (A,B,diff,bout);
 
 always @ (posedge clk)
 begin
-	if(i_clr || (i==256)) 
+	if(i_clr || (i == 256)) 
 	begin
 		i<=1'b0;
 		i_clr<=1'b0;
